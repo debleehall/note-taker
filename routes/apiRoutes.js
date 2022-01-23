b63ffa48e828d1f8f4e5f2b0039f9651643edd18
Debbie Hall <debleehall15@gmail.com>
2022-01-23 10:32:27 -0500

    trying different functions

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,24 +1,20 @@
 const { request } = require('express');
+const fs = require("fs");
+const { v4: uuidv4 } = require('uuid');
 
 const router = require('express').Router();
-// const res = require('express/lib/response');
-// const saveData = require('../db/notes');
 
 // GET /api/notes (return db.json)
-router.get('/notes', (req, res) => {
+router.get('/api/notes', (req, res) => {
     let results = notes;
     if (req.query) {
         results = filterByQuery(req.query, results);
     }
     res.json(results);
-    // saveData
-    //     .retrieveNotes()
-    //     .then(notes => res.json(notes))
-    //     .catch(err => res.status(500).json(err));
 });
 
 // POST /api/notes (save post to db.json)
-router.post('/notes', (req, res) => {
+router.post('/api/notes', (req, res) => {
     req.body.id = notes.length.toString();
 
     if (!validateNote(req.body)) {
@@ -27,20 +23,15 @@ router.post('/notes', (req, res) => {
         const note = createNewNote(re.body, note)
         res.json(note);
     }
-    // saveData
-    //     .addNote(req.body)
-    //     .then((note) => res.json(note))
-    //     .catch(err => res.status(500).json(err));
 });
 
 // Bonus: DELETE /api/notes/:id (removes a note from db.json)
-router.delete('/notes:id', (req, res) => {
+router.delete('/api/notes:id', (req, res) => {
     let noteId = request.params.id.toString();
-    
-    // saveData
-    //     .deleteNote(req.params.id)
-    //     .then(() => res.json({ ok:true }))
-    //     .catch(err => res.status(500).json(err));
+    let data = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+    const newData = data.filter( note => note.id.toString() !== noteId );
+    fs.writeFileSync('./db/db.json', JSON.stringify(newData));
+    response.json(newData);
 });
 
 module.exports = router;
