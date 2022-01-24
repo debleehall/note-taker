984596ca3146ec860126ad82f15a776678a72a05
Debbie Hall <debleehall15@gmail.com>
2022-01-23 21:21:43 -0500

    merging branches

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,39 +1,34 @@
 const { request } = require('express');
-const { filterByQuery, createNewNote, validateNote } = require('../lib/notes');
-const { notes } = require('../db/db.json');
 const fs = require("fs");
-const { v4: uuidv4 } = require('uuid');
 
 const router = require('express').Router();
 
+const noteList = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
+
 // GET /api/notes (return db.json)
 router.get('/api/notes', (req, res) => {
-    let results = notes;
-    // if (req.query) {
-    //     results = filterByQuery(req.query, results);
-    // }
-    res.json(results);
+    return res.json(noteList);
 });
 
 // POST /api/notes (save post to db.json)
 router.post('/api/notes', (req, res) => {
-    req.body.id = notes.length.toString();
-
-    if (!validateNote(req.body)) {
-        res.status(400).send('the not is not formatted correctly.');
+    let lastId;
+    if (noteList.length) {
+        lastId = Math.max(...(noteList.map(note => note.id)));
     } else {
-        const note = createNewNote(re.body, note)
-        res.json(note);
+        lastId = 0;
     }
+    const id = lastId = 1;
+
+    noteList.push({ id, ...req.body });
+    res.json(noteList.slice(-1));
 });
 
 // Bonus: DELETE /api/notes/:id (removes a note from db.json)
-router.delete('/api/notes:id', (req, res) => {
-    let noteId = request.params.id.toString();
-    let data = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
-    const newData = data.filter( note => note.id.toString() !== noteId );
-    fs.writeFileSync('./db/db.json', JSON.stringify(newData));
-    response.json(newData);
+router.delete('/api/notes/:id', (req, res) => {
+    let findNote = noteList.find(({ id }) => id === JSON.parse(req.params.id));
+    noteList.splice(noteList.indexOf(findNote), 1);
+    res.end("Note was deleted.");
 });
 
 module.exports = router;
